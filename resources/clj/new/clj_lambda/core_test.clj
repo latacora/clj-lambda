0a268838fab30c41f2ae208fd644619aad4952d4
Gabriel Horner <gabriel@latacora.com>
2021-12-08 15:25:07 -0500

    Fix test failure, update deps and add deps CI job

--- resources/clj/new/clj_lambda/core_test.clj
+++ resources/clj/new/clj_lambda/core_test.clj
@@ -1,12 +1,12 @@
 (ns {{namespace}}-test
   (:require [clojure.test :refer [deftest is]]
             [cheshire.core :as json]
-            [com.latacora.baz :as baz])
+            [{{namespace}} :as {{name}}])
   (:import [java.io StringWriter StringReader]))
 
 (deftest handle-request-test
   (let [out (StringWriter.)
         mock-event {:Records []}]
-    (baz/-handleRequest nil (StringReader. (json/generate-string mock-event)) out nil)
+    ({{name}}/-handleRequest nil (StringReader. (json/generate-string mock-event)) out nil)
     (is (= {"hello" "world"}
            (json/parse-string (str out))))))
